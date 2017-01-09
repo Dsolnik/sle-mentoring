36a0b86a8c9502b429af531d54ac3cdc8f6f9b2b
Stephen Jung Huh <stephenhuh@gmail.com>
2017-01-09 13:40:19 -0600

    Iteration 2

---
+++ iteration2/GetMenu/script.js
@@ -0,0 +1,27 @@
+var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send(null);
+  });
+};
+
+getJSON("http://demo3354820.mockable.io/menu/sushi").then(function(data) {
+    alert('Your Json result is:  ' + data.responseText); //you can comment this, i used it to debug
+
+    result.innerText = data.responseText; //display the result in an HTML element
+}, function(status) { //error detection....
+  alert('Something went wrong.');
+});
+
+
+
