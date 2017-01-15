3ae082c5188074e3f87f90524fc6112ee909570f
Steven Le <steventle110@gmail.com>
2017-01-15 17:39:30 -0500

    edits

--- iteration2/GetMenu/script.js
+++ iteration2/GetMenu/script.js
@@ -1,27 +1,44 @@
-var getJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send(null);
+// Using jQuery
+
+//When content has finished loading, execute this lambda function
+$(function() {
+  //Lets look for html with a class with .button and then attach an event handler
+  $(".button").on("click", function(e) {
+    //On click, we dont want default behavior of the browser
+    e.preventDefault();
+    //Sending a get request to url, after you get data...
+    $.get("http://demo3354820.mockable.io/menu/sushi", function(data){
+      //getting html with .result as the class, then replacing it with data return
+      data = JSON.stringify(data);
+      console.log(data);
+      // accessing the #result div and appending the html into it
+      $("#result").html(data);//text(data.menu["Dragon Roll"]);
+      alert( "Data load successful");
+    });
+    //
   });
-};
-
-getJSON("http://demo3354820.mockable.io/menu/sushi").then(function(data) {
-    alert('Your Json result is:  ' + data.responseText); //you can comment this, i used it to debug
-
-    result.innerText = data.responseText; //display the result in an HTML element
-}, function(status) { //error detection....
-  alert('Something went wrong.');
 });
 
 
+// ... Attempt at converting above to constructor format ...
+
+
+
+
+// Constructor: GET Request to URL
+function getJSON(url) {
+  return { 
+    data: function(url) {
+      $.get(url, function(data){
+        data = JSON.stringify(data);
+      });
+    },
+    url: url
+  };
+}
+
+// // Constructor: stringify JSON
+// function JSONstringify(data){
+//   data = JSON.stringify(data);
+// }
 
