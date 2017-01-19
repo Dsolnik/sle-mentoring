a3c829b69f4947572866470b716f32c1fd546d60
STEVEN LE <steventle110@gmail.com>
2017-01-19 11:59:32 -0500

    more edits

--- iteration2/GetMenu/script.js
+++ iteration2/GetMenu/script.js
@@ -32,34 +32,58 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+// --------------------- My Version Using jQuery ------------------------------
+$(function(){
+  
+  url = "http://demo3354820.mockable.io/menu/sushi";
+
+  // define get Request
+  var getRequest = function(url){
+    $.get(url, function(data){
+      data = JSON.stringify(data);
+      alert("successful data retrieve");
+      console.log(data);
+      $("#result").html(data);
+      alert("changed posted");
+    });
+  };
+
+  // Defining behavior
+  var clickHandler = function(cb) {
+    $("#menuButton").on('click', cb);
+  }
+
+  clickHandler(getRequest(url));
+
+});
 
 
 
-// --------------------- Using jQuery ------------------------------
+// --------------------- Stevie's Version Using jQuery ------------------------------
 // Make sure DOM is ready before any manipulation occurs
-// $(function(){
+$(function(){
 
-//   // Defining behavior
-//   let myModule = {
-//     addClickHandler : function(cb) {
-//       $(".button").on('click', cb);
-//       console.log("addClickHandler successful");
-//     },
-//     sendGetRequest : function(url) {
-//       return function(){
-//         $.get(url, function(data){
-//           data = JSON.stringify(data);
-//           console.log(data);
-//         $("#result").html(data);
-//         alert( "Data load success");  
-//         });
-//       } 
-//     }
-//   }
+  // Defining behavior
+  let myModule = {
+    addClickHandler : function(cb) {
+      $(".button").on('click', cb);
+      console.log("addClickHandler successful");
+    },
+    sendGetRequest : function(url) {
+      return function(){
+        $.get(url, function(data){
+          data = JSON.stringify(data);
+          console.log(data);
+        $("#result").html(data);
+        alert( "Data load success");  
+        });
+      } 
+    }
+  }
 
-//   // Using Behavior
-//   myModule.addClickHandler(myModule.sendGetRequest("http://demo3354820.mockable.io/menu/sushi"));
+  // Using Behavior
+  myModule.addClickHandler(myModule.sendGetRequest("http://demo3354820.mockable.io/menu/sushi"));
 
-// });
+});
 
 
