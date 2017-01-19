227496bd0b16e0cb7f8cf79f44200e8fdddc12b5
STEVEN LE <steventle110@gmail.com>
2017-01-19 11:42:43 -0500

    accomplished with vanilla js

--- iteration2/GetMenu/script.js
+++ iteration2/GetMenu/script.js
@@ -1,25 +1,65 @@
-// Make sure DOM is ready before any manipulation occurs
-$(function(){
+// --------------------- Using Vanilla JS ------------------------------
 
-  // Defining behavior
-  let myModule = {
-    addClickHandler : function(cb) {
-      $(".button").on('click', cb);
-      console.log("addClickHandler successful");
-    },
-    sendGetRequest : function(url) {
-      return function(){
-        $.get(url, function(data){
-          data = JSON.stringify(data);
-          console.log(data);
-        $("#result").html(data);
-        alert( "Data load success");  
-        });
-      } 
-    }
-  }
+document.addEventListener("DOMContentLoaded", function() {
 
-  // Using Behavior
-  myModule.addClickHandler(myModule.sendGetRequest("http://demo3354820.mockable.io/menu/sushi"));
+  let getMenuButtonActions = (function() {
+    url = "http://demo3354820.mockable.io/menu/sushi";
+    // Define getRequest
+    var getRequest = function(url, cb) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = function () { cb(null, xhr.response); };
+      xhr.onerror = function () { cb(xhr.response); };
+      xhr.send();
+    };
+    // Define click handler
+    var clickHandler = function() {
+      getRequest(url, function(err, data){
+        if (err) { throw err; }
+        alert(" Data load success ");
+        data = JSON.stringify(data);
+        // when data is retrieved update div
+        var resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = data;
+      });
+
+    };
+    // append click handler to .onclick event listener
+    var button = document.getElementById("menuButton");
+    button.onclick = clickHandler;
+  })();
+
 
 });
+
+
+
+
+// --------------------- Using jQuery ------------------------------
+// Make sure DOM is ready before any manipulation occurs
+// $(function(){
+
+//   // Defining behavior
+//   let myModule = {
+//     addClickHandler : function(cb) {
+//       $(".button").on('click', cb);
+//       console.log("addClickHandler successful");
+//     },
+//     sendGetRequest : function(url) {
+//       return function(){
+//         $.get(url, function(data){
+//           data = JSON.stringify(data);
+//           console.log(data);
+//         $("#result").html(data);
+//         alert( "Data load success");  
+//         });
+//       } 
+//     }
+//   }
+
+//   // Using Behavior
+//   myModule.addClickHandler(myModule.sendGetRequest("http://demo3354820.mockable.io/menu/sushi"));
+
+// });
+
+
